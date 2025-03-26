18619ea5076eac14af1d43dce37b6da076caec81
Ezequiel Baamonde <68140436+ezequielbaamonde@users.noreply.github.com>
2025-03-26 08:44:44 -0300

    Comentarios index.php

--- src/public/index.php
+++ src/public/index.php
@@ -1,17 +1,18 @@
 <?php
-
+/* DEPENDENCIAS IMPORTADAS DEL PSR de SLIM */
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '.../vendor/autoload.php'; //COLOCAR PUNTOS PARA DIRIGIRME AL DIRECTORIO VENDOR.
 
-$app = AppFactory::create();
+$app = AppFactory::create(); //Crea la app (El Core)
 
-$app->get('/', function (Request $request, Response $response, $args) {
+$app->get('/', function (Request $request, Response $response, $args) { //El string del argumento es el LOCALHOST de la APP.
     $response->getBody()->write("Hello world!");
     return $response;
 });
 
-$app->run();
+$app->run(); //Corre la APP.
+/* No es necesario cerrar la etiqueta PHP para ejecutar este codigo */
 
